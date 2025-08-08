f8df51b570a522593e9ae724a197e86dbc4708d7
wasim3399 <wasim.rashid@developers.studio>
2025-08-08 16:16:44 +0500

    added about

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/about', function () {
+    dd('about');
+})->name('about');
+
 require __DIR__.'/auth.php';
